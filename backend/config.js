bf0afc4061f5936f04227a14c948f0845e46bacb
ravinder <ravirangera0@gmail.com>
2023-09-10 15:09:58 +0530

    Intial commit

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 export const mongoDBURL = 
-'mongodb+srv://ravinder:learnMongoDB@cluster0.x2tw71f.mongodb.net/books-collection?retryWrites=true&w=majority';
+'mongodb+srv://ravirangera0:buildBookStore@cluster0.cucyncj.mongodb.net/?retryWrites=true&w=majority';
 
